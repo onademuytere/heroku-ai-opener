97765002d98a00b06bbd828eb1648b765dee9c47
Ona Demuytere <onademuy@student.arteveldehs.be>
2023-04-10 15:31:51 +0200

    loginscreen success

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
 from authentication.firestore_functions import *
+from flask import Flask, session, render_template, request, redirect, jsonify, url_for
+from jinja2 import TemplateNotFound
+from authentication.firestore_authentication import pb, auth
+from decouple import config
+from authentication import blueprint
+from authentication.forms import LoginForm, CreateAccountForm
+import os
+
+
 
 app = Flask(__name__)
+app.secret_key = config("APP_SECRET_KEY")
 
 @app.route('/')
 def home():
     return "Flask heroku atespp"
+
 
 @app.route('/hello/<name>')
 def hello(name=None):
@@ -17,9 +27,33 @@ def rooms():
     rooms = getRooms()
     return render_template('home/rooms.html', segment='rooms', rooms=rooms)
 
-@app.route('/base')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return "test"
+    login_form = LoginForm(request.form)
+    if 'login' in request.form:
+        try:
+            email = request.form['username']
+            password = request.form['password']
+            auth.sign_in_with_email_and_password(email, password)
+            session['user'] = email
+            print(session)
+            return hello("Ona")
+
+        # Something (user or pass) is not ok
+        except:
+            return render_template('accounts/login.html',
+                                    msg='Wrong email or password',
+                                    form=login_form)
+
+    if 'user' in session:
+        return rooms()
+
+    return render_template('accounts/login.html', form=login_form)
+
+
+
+
 
 
 if __name__ == "__main__":
